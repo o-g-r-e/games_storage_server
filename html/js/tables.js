2b81be119155e6f922164a1b5a5d1f95dbd6d0ee
ydroba <ydroba@wiley.com>
2019-09-12 15:48:41 +0300

    frontend dev

--- html/js/tables.js
+++ html/js/tables.js
@@ -28,9 +28,9 @@ $(function() {
 	$(htmlTable).appendTo('.container');
 
 	function buildTable(tableName, tableData) {
-		var htmlTable = '<div class="row">';
+		var htmlTable = '<div class="row usr-table-block">';
 		htmlTable += '<div class="col-md-12 col-sm-6">';
-		htmlTable += '<h3>'+tableName+'</h3>';
+		htmlTable += '<h3 style="float:left">'+tableName+'</h3><button type="button">Edit '+tableName+'</button>';
 		htmlTable += '<table id="'+tableName.toLowerCase()+'-tb" class="table">';
 		htmlTable += '<thead>';
 		htmlTable += '<tr>';
@@ -54,7 +54,8 @@ $(function() {
 		htmlTable += '</tbody>';
 		htmlTable += '</table>';
 		htmlTable += '</div>';
-		htmlTable += '<button id="add-row-to-'+tableName.toLowerCase()+'" type="button" class="btn btn-primary">Add row</button>';
+		htmlTable += '<button id="add-row-to-'+tableName.toLowerCase()+'" type="button" class="btn btn-primary add-row-btn">Add row</button>';
+		htmlTable += '<button id="sbm-new-rows-'+tableName.toLowerCase()+'" style="margin-left: 4px; display: none;" type="button" class="btn btn-success">Submit</button>';
 		htmlTable += '</div>';
 
 		return htmlTable;
@@ -64,6 +65,19 @@ $(function() {
 		$('.modal-table-creation').show();
 	});
 
+	$('.add-row-btn').on('click', function() {
+		var tableId = /add-row-to-(.*)/.exec($(this).attr('id'))[1];
+		var lastRow = $("#"+tableId+"-tb tbody tr:last");
+		var newRow = '<tr class="nrow">';
+		$(lastRow).children("td").each(function (index, value) {
+			newRow += '<td><input type="text" name="val"></td>';
+		});
+		newRow += '</tr>';
+		$(newRow).insertAfter(lastRow);
+		$("#"+tableId+"-tb tbody tr:last td:last").append('<button id="remove-new-row" style="float: right;">Remove</button>');
+		$("#sbm-new-rows-"+tableId).show();
+	});
+
 	$('#table-creation-modal-close-button1, #table-creation-modal-close-button2').on('click', function() {
 		$('.modal-table-creation').hide();
 		$('.modal-table-creation .additional-field').remove();
@@ -71,22 +85,29 @@ $(function() {
 	});
 
 	$('#add-field').on('click', function() {
-		var el = '<div class="input-group additional-field">';
-		el += '<span class="input-group-addon">Type:</span>';
-		el += '<select class="form-control" id="sel1">';
+		var el = '<div class="input-group nfield additional-field">';
+		el += '<label>Type:\n';
+		el += '<select name="f1-type">';
 		el += '<option>INTEGER</option>';
 		el += '<option>STRING</option>';
 		el += '<option>FLOAT</option>';
-		el += '</select>';
-		el += '<span class="input-group-addon">Name:</span>';
-		el += '<input type="text" class="form-control" name="val" placeholder="Val">';
-		el += '<button class="rem-field" type="button">X</button>';
+		el += '</select></label>\n';
+		el += '<label>Name:\n';
+		el += '<input type="text" name="f1-name" placeholder="Val"></label>';
+		el += '&nbsp;<button class="rem-field" type="button">Remove</button>';
 		el += '</div>';
 		$(el).insertBefore('.add-field-group');
 	});
 
 	$('.modal-body').on('click', '.rem-field', function() {
 		$(this).parent().remove();
+	});
+
+	$('.usr-table-block').on('click', 'div table tbody tr td #remove-new-row', function() {
+		if($(this).closest("tbody").children("tr.nrow").length <= 1) {
+			$(this).closest(".usr-table-block").children("[id^='sbm-new-rows-']").hide();
+		}
+		$(this).parent().parent().remove();
 	});
 	
 	function buildTableRows(tableElement, table) {
